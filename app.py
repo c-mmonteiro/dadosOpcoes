b6ee416eb4f9e8d73ef8faf5c098724e23a7491d
Carlos Monteiro <invest.monteiro@gmail.com>
2023-01-30 12:37:03 -0300

    changes on app.py

--- app.py
+++ app.py
@@ -72,11 +72,11 @@ def interval_update(intervalo, n_click, min, max):
     
     dados, base_last = monitor.atualiza()
     fig = make_subplots(rows=1, cols=1, subplot_titles=('Plot 1'))
-    fig.add_trace(go.Line(x=dados["Strike"], y=dados["IA"], name="IA", line=dict(color='black')))
     fig.add_trace(go.Line(x=dados["Strike"], y=dados["Compra"], name="Compra", line=dict(color='red')))
-    fig.add_trace(go.Line(x=dados["Strike"], y=dados["Venda"], name="Venda", line=dict(color='green')))
     fig.add_trace(go.Line(x=dados["Strike"], y=dados["Ultimo"], name="Ultimo", line=dict(color='blue')))
     fig.add_trace(go.Line(x=[base_last, base_last], y=[dados["IA"].min(), dados["IA"].max()] , name="Ativo", line=dict(color='red')))
+    fig.add_trace(go.Line(x=dados["Strike"], y=dados["IA"], name="IA", line=dict(color='black')))
+    fig.add_trace(go.Line(x=dados["Strike"], y=dados["Venda"], name="Venda", line=dict(color='green')))
     now = datetime.now()
     date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
     
